6fa9e9c60751a15f6266ced01c4a25c07418467c
Athena <atpa1875@colorado.edu>
2020-10-11 18:16:43 -0600

    factory

--- factory.java
+++ factory.java
@@ -1,3 +1,31 @@
 class factory { //factory pattern
     factory () {}
+    public Roll createRoll (String rollType) {
+        if (rollType == "egg") {
+            return new eggroll();
+        }
+        if (rollType == "spring") {
+            return new springroll();
+        }
+        if (rollType == "pastry") {
+            return new pastryroll();
+        }
+        if (rollType == "sausage") {
+            return new sausageroll();
+        }
+        if (rollType == "jelly") {
+            return new jellyroll();
+        }
+    }
+    public Customer createCustomer (String customerType) {
+        if (customerType == "casual") {
+            return new casualcustomer();
+        }
+        if (customerType == "business") {
+            return new businesscustomer();
+        }
+        if (customerType == "catering") {
+            return new cateringcustomer();
+        }
+    }
 }
