e005ac510f341eb80f9b2a09775ce9addaa675a2
Athena <atpa1875@colorado.edu>
2020-10-11 19:55:58 -0600

    observer

--- customer.java
+++ customer.java
@@ -1,5 +1,19 @@
-public abstract class customer {
-    customer () {}
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public abstract class customer extends PropertyChangeSupport implements PropertyChangeListener{
+    public customer (bean sourcebean) {
+        super(sourcebean);
+    }
+    public void arrive () {}
+    public void leave () {}
     public void buyRolls () {}
     public void rollsOut () {}
+
+    public void propertyChange(PropertyChangeEvent event) { //whenever event changes, zoo announcer announces.
+        if (event.getOldValue() != event.getNewValue()) {
+            leave();
+        }
+    }
 }
